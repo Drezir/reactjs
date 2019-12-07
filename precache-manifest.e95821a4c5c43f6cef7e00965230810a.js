32793820134c14689689487123f29449a9051fd1
Adam Ostrožlík <adamostrozlik@yandex.com>
2019-12-07 18:50:10 +0100

    Updates

---
+++ precache-manifest.e95821a4c5c43f6cef7e00965230810a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cff1f5f1093cb363ccd0c64fedcd2c",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "61ae6a84cdf2232725ed",
+    "url": "/reactjs/static/css/main.b144c58b.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/reactjs/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "61ae6a84cdf2232725ed",
+    "url": "/reactjs/static/js/main.31ead809.chunk.js"
+  },
+  {
+    "revision": "307c4067df827d21aaab",
+    "url": "/reactjs/static/js/runtime-main.0095999c.js"
+  }
+]);
