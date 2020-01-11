1824cfb147b91ea0a196d5dc56f4e02fac4d0800
Adam Ostrožlík <adamostrozlik@yandex.com>
2020-01-11 15:10:07 +0100

    No logging for deployment

--- crwn-clothing/src/redux/store.js
+++ crwn-clothing/src/redux/store.js
@@ -10,7 +10,11 @@ const composeEnhancers =
             // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
         }) : compose;
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+    middlewares.push(logger);
+}
 
 const enhancer = composeEnhancers(
     applyMiddleware(...middlewares),
